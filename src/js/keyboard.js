e128fb87921f1115731e04948db0d11bb5e3388d
hdkim <eastshine94@naver.com>
2023-03-25 23:41:47 +0900

    feat: keyboard event 적용

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -3,6 +3,10 @@ export class Keyboard {
   #containerEl;
   #swichEl;
   #fontSelectEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
+
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -12,13 +16,37 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
     this.#swichEl.addEventListener("change", this.#onChangeTheme);
-
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
   }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-힣]/, "");
+  }
+
+  #onKeyDown(event) {
+    this.#inputGroupEl.classList.toggle("error", /[ㄱ-힣]/.test(event.key));
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  }
+
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
+  }
+
   #onChangeTheme(event) {
     const { checked } = event.target;
     document.documentElement.setAttribute("theme", checked ? "dark-mode" : "");
