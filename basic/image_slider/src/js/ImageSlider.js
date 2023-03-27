eb18ce69a91b1dd43bf017c1322b2905259dd822
hdkim <eastshine94@naver.com>
2023-03-27 23:02:48 +0900

    feat: indicator 기능 구현

--- basic/image_slider/src/js/ImageSlider.js
+++ basic/image_slider/src/js/ImageSlider.js
@@ -13,10 +13,14 @@ export default class ImageSlider {
 
   prevBtnEl;
 
+  indicatorWrapEl;
+
   constructor() {
     this.assignElement();
     this.init();
     this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
   }
 
   assignElement() {
@@ -24,6 +28,7 @@ export default class ImageSlider {
     this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
     this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
     this.prevBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
   }
 
   initSliderNumber() {
@@ -49,6 +54,10 @@ export default class ImageSlider {
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
     this.prevBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
   }
 
   moveToRight() {
@@ -59,6 +68,7 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#sliderWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
   }
 
   moveToLeft() {
@@ -69,5 +79,34 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#sliderWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
+  }
+
+  createIndicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#sliderNumber; i += 1) {
+      const li = document.createElement('li');
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setIndicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      ?.classList.add('active');
+  }
+
+  onClickIndicator(event) {
+    const indexPosition = Number(event.target.dataset.index);
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#sliderWidth * this.#currentPosition
+      }px`;
+      this.setIndicator();
+    }
   }
 }
