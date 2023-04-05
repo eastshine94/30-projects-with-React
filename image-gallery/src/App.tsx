1709a0ff573840abbdbd9ffe164f5a57c46b3ee8
hdkim <eastshine94@naver.com>
2023-04-06 00:17:10 +0900

    feat: 이미지 갤러리 > 드래그 드롭 사용

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -1,11 +1,22 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import ImageBox from "./components/ImageBox";
+import { useDropzone } from "react-dropzone";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
   const [imageList, setImageList] = useState<string[]>([]);
-
+  const onDrop = useCallback((acceptedFiles: any) => {
+    if (acceptedFiles.length) {
+      for (const file of acceptedFiles) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = (event) => {
+          setImageList((prev) => [...prev, event.target?.result as string]);
+        };
+      }
+    }
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
   return (
     <div className="container">
       <div className={`gallery-box ${imageList.length > 0 ? "row" : ""}`}>
@@ -15,35 +26,13 @@ function App() {
             이미지를 추가해주세요.
           </div>
         )}
-        <input
-          type="file"
-          ref={inputRef}
-          onChange={(event) => {
-            const file = event.currentTarget.files?.[0];
-            if (file) {
-              const reader = new FileReader();
-              reader.readAsDataURL(file);
-              reader.onloadend = (event) => {
-                setImageList((prev) => [
-                  ...prev,
-                  event.target?.result as string,
-                ]);
-              };
-            }
-          }}
-        />
 
         {imageList.map((val, idx) => (
           <ImageBox key={val + idx} src={val} />
         ))}
 
-        <div
-          className="plus-box"
-          onClick={() => {
-            inputRef.current?.click();
-          }}
-        >
-          +
+        <div className="plus-box" {...getRootProps()}>
+          <input {...getInputProps()} />+
         </div>
       </div>
     </div>
