d6998250e63fd84ea0327e1681c6122d17fdd77a
hdkim <eastshine94@naver.com>
2023-04-10 23:35:42 +0900

    feat: 다운로드 기능

--- instermediate/drawing-board/src/js/index.js
+++ instermediate/drawing-board/src/js/index.js
@@ -5,6 +5,23 @@ class DrawingBoard {
   backgroundColor = "#FFFFFF";
   isNavigatorVisible = false;
   undoArray = [];
+
+  containerEl;
+  canvasEl;
+  toolbarEl;
+  brushEl;
+  colorPickerEl;
+  brushPanelEl;
+  brushSliderEl;
+  brushSizePreviewEl;
+  eraserEl;
+  navigatorEl;
+  navigatorImageContainerEl;
+  navigatorImageEl;
+  undoEl;
+  clearEl;
+  downloadLinkEl;
+
   constructor() {
     this.assignElement();
     this.initContext();
@@ -29,6 +46,7 @@ class DrawingBoard {
       this.navigatorImageContainerEl.querySelector("#canvasImg");
     this.undoEl = this.toolbarEl.querySelector("#undo");
     this.clearEl = this.toolbarEl.querySelector("#clear");
+    this.downloadLinkEl = this.toolbarEl.querySelector("#download");
   }
   initContext() {
     this.context = this.canvasEl.getContext("2d");
@@ -56,7 +74,16 @@ class DrawingBoard {
     );
     this.undoEl.addEventListener("click", this.onClickUndo.bind(this));
     this.clearEl.addEventListener("click", this.onClickClear.bind(this));
+    this.downloadLinkEl.addEventListener(
+      "click",
+      this.onClickDownload.bind(this)
+    );
   }
+  onClickDownload() {
+    this.downloadLinkEl.href = this.canvasEl.toDataURL("image/jpeg", 1);
+    this.downloadLinkEl.download = "example.jpeg";
+  }
+
   onClickClear() {
     this.context.clearRect(0, 0, this.canvasEl.width, this.canvasEl.height);
     this.undoArray = [];
