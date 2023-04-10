37f28822eca9e303ab5d7d7ddd71950ad04dcf3f
hdkim <eastshine94@naver.com>
2023-04-11 00:09:17 +0900

    chore: 개발환경 설정 - Rollup

---
+++ instermediate/todo-list/rollup.prd.config.js
@@ -0,0 +1,8 @@
+import rollupCommonConfig from "./rollup.common.config";
+import { terser } from "rollup-plugin-terser";
+
+const config = { ...rollupCommonConfig };
+
+config.plugins = [...config.plugins, terser()];
+
+export default config;
