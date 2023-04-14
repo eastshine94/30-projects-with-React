761621ca81c2db4875109c670da1d6c9c2d9864c
hdkim <eastshine94@naver.com>
2023-04-14 23:04:50 +0900

    feat: 필터기능

--- instermediate/todo-list/src/js/index.js
+++ instermediate/todo-list/src/js/index.js
@@ -12,6 +12,7 @@ class TodoList {
   constructor() {
     this.assignElement();
     this.addEvent();
+    this.addRadioBtnEvent();
   }
   assignElement() {
     this.inputContainerEl = document.getElementById("input-container");
@@ -21,11 +22,51 @@ class TodoList {
 
     this.todoContainerEl = document.getElementById("todo-container");
     this.todoListEl = this.todoContainerEl.querySelector("#todo-list");
+    this.radioAreaEl = this.inputContainerEl.querySelector("#radio-area");
+    this.filterRadioBtnEls = this.radioAreaEl.querySelectorAll(
+      "input[name='filter']"
+    );
   }
   addEvent() {
     this.addBtnEl.addEventListener("click", this.onClickAddBtn.bind(this));
     this.todoListEl.addEventListener("click", this.onClickTodoList.bind(this));
   }
+
+  addRadioBtnEvent() {
+    for (const filterRadioBtnEls of this.filterRadioBtnEls) {
+      filterRadioBtnEls.addEventListener(
+        "click",
+        this.onClickRadioBtn.bind(this)
+      );
+    }
+  }
+
+  onClickRadioBtn(event) {
+    const { value } = event.target;
+    this.filterTodo(value);
+  }
+
+  filterTodo(status) {
+    const todoDivEls = this.todoListEl.querySelectorAll("div.todo");
+    for (const todoDivEl of todoDivEls) {
+      switch (status) {
+        case "ALL":
+          todoDivEl.style.display = "flex";
+          break;
+        case "DONE":
+          todoDivEl.style.display = todoDivEl.classList.contains("done")
+            ? "flex"
+            : "none";
+          break;
+        case "TODO":
+          todoDivEl.style.display = todoDivEl.classList.contains("done")
+            ? "none"
+            : "flex";
+          break;
+      }
+    }
+  }
+
   onClickTodoList(event) {
     const { target } = event;
     const btn = target.closest("button");
